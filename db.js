c4d0dd8c764f520c4d2422a2289fd66de68af734
Uday <uadeshaysaini@outlook.com>
2023-11-27 14:00:19 +0530

    added Atlas

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://0.0.0.0:27017/inotebook?readPreference=primary&directConnection=true&tls=false"
+const mongoURI = "mongodb+srv://uadeshaysaini:TDeDUkfsGvYB4sHD@cluster0.uiek9c6.mongodb.net/MyNotebook?retryWrites=true&w=majority"
 
 async function connectToMongo() {
     await mongoose.connect(mongoURI).then(()=> console.log("Connected to Mongo Successfully")).catch(err => console.log(err));
